7a50005ebc73b87486dfd771401fbda3d58ba2b0
Alex Cubero <alexcuar@gmail.com>
2018-06-24 17:20:13 +0200

    Era necesario poner un mensaje al comienzo

--- go-project.go
+++ go-project.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-
+	fmt.Println("Este mensaje nos alerta de que hemos entrado en el programa")
 	cmd := exec.Command("C:\\Users\\Lenovo\\Projects\\go\\goproject\\goproject\\mycd.bat")
 	thiserr := cmd.Run()
 	if thiserr != nil {
